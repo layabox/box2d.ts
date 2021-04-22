733c62dc0106df44439bba9cc8fb0368a83d6da6
Lurenyiabc <934555872@qq.com>
2021-04-22 17:21:09 +0800

    编译

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "src/index.ts",
+  input: "./src/index.ts",
   output: {
-    file: "./dist/box2d.umd.js",
-    name: "b2",
-    format: "umd",
-    sourcemap: true
+    file: "./dist/box2d.js",
+    name: "box2d",
+    format: "iife",
+    sourcemap: true,
   },
   plugins: [
     typescript({ clean: true, tsconfigOverride: { compilerOptions: { target: "ES2015", module: "ES2015", declaration: false } } }),
